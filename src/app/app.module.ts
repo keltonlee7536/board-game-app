d1a3a94b46c7bd9b9c4cb0567a0ec02318a3e5ef
keltonlee7536 <kelton.lee7536@stu.mtec.edu>
2023-04-17 09:10:45 -0600

    ng board-game-app pt 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,27 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './modules/app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SideNavComponent } from './components/side-nav/side-nav.component';
+import { GameSearchComponent } from './components/game-search/game-search.component';
+import { GameDetailsComponent } from './components/game-details/game-details.component';
+import { GameDetailControlsComponent } from './components/game-detail-controls/game-detail-controls.component';
+import { UserSelectionsComponent } from './components/user-selections/user-selections.component';
+import { MaterialModule } from './modules/material/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent,
+    GameSearchComponent,
+    GameDetailsComponent,
+    GameDetailControlsComponent,
+    UserSelectionsComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/game-detail-controls/game-detail-controls.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-game-detail-controls',
+  templateUrl: './game-detail-controls.component.html',
+  styleUrls: ['./game-detail-controls.component.scss']
+})
+export class GameDetailControlsComponent {
+
+}
