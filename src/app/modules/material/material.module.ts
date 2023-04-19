90710f54e4ac4fddb175ac69b05a9ebbc1ddd2d3
keltonlee7536 <kelton.lee7536@stu.mtec.edu>
2023-04-19 08:22:26 -0600

    part-3-application-shell/pt2-side-nav complete. works

--- src/app/modules/material/material.module.ts
+++ src/app/modules/material/material.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatIconModule } from '@angular/material/icon';
 
 
 @NgModule({
@@ -9,11 +11,15 @@ import { MatSidenavModule } from '@angular/material/sidenav';
   imports: [
     CommonModule,
     MatToolbarModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule
   ],
   exports: [
     MatToolbarModule,
-    MatSidenavModule
+    MatSidenavModule,
+    MatListModule,
+    MatIconModule
   ]
 })
 export class MaterialModule { }
